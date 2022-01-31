bc0bea998bff02735000992efea84535957e7280
William Warner Burke <williamburke121@gmail.com>
2022-01-31 18:28:15 -0500

    commit tree 67f24d1353d493f83c2b6d8fe32858027baa4519

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+const Hq = 42
+const feet = 12
+function distanceFromHqInBlocks(blocks){
+    if (Hq < blocks){
+        return blocks - Hq
+    } else (Hq > blocks)
+  return Hq - blocks
+
+}
+function distanceFromHqInFeet(blocks) {
+    return distanceFromHqInBlocks(blocks) * 264
+
+}
+function distanceTravelledInFeet(start, destination){
+    return Math.abs(start - destination) * 264
+}
+function calculatesFarePrice(start, destination) {
+  if (distanceTravelledInFeet(start, destination) <= 400){
+      return 0;
+  } else if (distanceTravelledInFeet(start, destination) <=2000){
+      return (distanceTravelledInFeet(start, destination) - 400) * .02;
+  }else if (distanceTravelledInFeet(start, destination) <= 2500) {
+      return 25
+  } else{
+      return 'cannot travel that far';
+  }
+}
+
